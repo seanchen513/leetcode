03225206bd1d258d3f5a910e7443d58cde8b64fd
Sean Chen <seanchen513@gmail.com>
2020-02-18 03:10:08 -0800

    update 812

--- math/0812_largest_triangle_area.py
+++ math/0812_largest_triangle_area.py
@@ -165,30 +165,56 @@ Solution 3: find convex hull first.  Then use Shoelace formula on points on it.
 
 On LC, this is about 10 times faster than not using the convex hull.
 
+https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
+
 Runtime: 28 ms, faster than 100.00% of Python3 online submissions
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions
 """
 class Solution3:
 	def largestTriangleArea(self, points: List[List[int]]) -> float:
 		def boundary(points):
+			## Use set() to remove duplicates.
+			## Convert each point from list to tuple to make them hashable,
+			## so that set() can be applied.  Convert set to list so we
+			## can use reversed(points) for building upper hull.
+			#points = list(set(map(tuple, points)))
+			
+			# Replaced line above because it's slow on LC, probably because
+			# of huge number of points in some test cases.  It might be ok
+			# if the input was given as a list of tuples instead.
+			# Apply set() in return statement instead.
 			points = sorted(points)
 
+			## Don't need this part for LC812 since len(points) >= 3
+			#if len(points) <= 1:
+			#	return points
+
+			# 2D cross product of OA and OB vectors, ie, z-component of
+			# their 3D cross product.  Positive if OAB is CCW, negative if
+			# CW, and 0 if collinear.
 			def cross(o, a, b): 
 				return (o[0]-a[0]) * (o[1]-b[1]) - (o[1]-a[1]) * (o[0]-b[0])
 			
+			# Build lower hull.
 			lower = []
 			for p in points:
 				while len(lower) >= 2 and cross(lower[-2], lower[-1], p) < 0: 
 					lower.pop()
 				lower.append(tuple(p))
 			
+			# Build upper hull.
 			upper = []
 			for p in reversed(points):
 				while len(upper) >= 2 and cross(upper[-2], upper[-1], p) < 0: 
 					upper.pop()
 				upper.append(tuple(p))
 			
-			return list(set(lower[:-1] + upper[:-1]))
+			## Last point of each list is omitted because it's repeated at
+			## the beginning of the other list.
+			#return lower[:-1] + upper[:-1]
+
+			# Use set() to remove duplicate points.
+			return set(lower[:-1] + upper[:-1]) # or convert to list if needed
 
 		p = boundary(points)
 		res = 0
