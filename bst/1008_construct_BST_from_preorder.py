4b88d8e89588e5a0d4d7313d1ce92a73f6bad7a4
Sean Chen <seanchen513@gmail.com>
2020-04-20 22:36:50 -0700

    update 1008

--- bst/1008_construct_BST_from_preorder.py
+++ bst/1008_construct_BST_from_preorder.py
@@ -20,27 +20,78 @@ The values of preorder are distinct.
 import sys
 sys.path.insert(1, '../tree/')
 
-from binary_tree import TreeNode, print_tree, array_to_bt_lc
+from binary_tree import TreeNode, print_tree
 from typing import List
+import bisect
 
 ###############################################################################
 """
-Solution: preorder recursion.
+Solution: preorder recursion on upper bound for subtree.
+
+Left children are bounded by the value of their parents.
+Right children share the same bound that their parents have.
+
+O(n) time
+O(h) extra space for recursion
+O(n) extra space for BST being built
+"""
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        def build(bound=float('inf')):
+            nonlocal index
+
+            if index == n or preorder[index] > bound:
+                return None
+
+            root = TreeNode(preorder[index])
+
+            index += 1
+
+            root.left = build(root.val) 
+            root.right = build(bound)
+
+            return root
+
+        n = len(preorder)
+        index = 0
+
+        return build()
+
+"""
+Solution 1b: same, but pass index as parameter, and return it along with node.
+"""
+class Solution1b:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        def build(index, bound=float('inf')):
+            if index == n or preorder[index] > bound:
+                return index, None
+
+            root = TreeNode(preorder[index])
+
+            index += 1
+
+            index, root.left = build(index, root.val) 
+            index, root.right = build(index, bound)
+
+            return index, root
+
+        n = len(preorder)
+
+        return build(0)[1]
+
+###############################################################################
+"""
+Solution 2: preorder recursion on start and end indices of preorder array
+for current subtree.
 
 First element of preorder is the root.  Then come the values for the left
 subtree, which are all smaller than the root.  Then come the values for the 
 right subtree, which are all bigger than the root.
 
 O(n log n) time if use binary search and pass around array indices
-O(n^2) time if use linear search or pass around array indices, or both.
 
 O(h) extra space for recursion
 O(n) extra space for BST that is built
-
-If used linear search instead of binary search:
-    O(n) for first search.
-    This is done once for every node that is constructed.
-    So total time is at most O(n^2).
 
 Binary search
     O(log n) for first search.
@@ -51,83 +102,85 @@ If pass around array copies (slicing), and used binary search:
     Copying for left and right halves together is O(n).
     This is done once for every node that is constructed.
     So total time for array copying is at most O(n^2).
-"""
-import bisect
 
-class Solution:
-    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        def build(arr, start, end):
-            if start > end:
-                return None
-            
-            root_val = arr[start]
-            root = TreeNode(root_val)
-            
-            ### linear search for index of first number bigger than root
-            #i = start + 1
-            #while (i <= end) and (arr[i] < root_val):
-            #    i += 1
-
-            ### binary search for first element > root_val within range
-            ### specified by indices start+1, end+1 (Python slice convention)
-            i = bisect.bisect(arr, root_val, start + 1, end + 1)
-            
-            root.left = build(arr, start + 1, i - 1)
-            root.right = build(arr, i, end)
-        
-            return root
-        
-        return build(preorder, 0, len(preorder)-1)
-
-###############################################################################
-"""
-Solution 2: preorder recursion using upper bounds.
-
-O(n) time
-O(h) extra space for recursion
-O(n) extra space for BST being built
+Note: can pass around modified "end" index that is 1 more than regular one.
 """
 class Solution2:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        def build(arr, bound=float('inf')):
-            nonlocal index
-
-            if index == n or arr[index] > bound:
+        def build(start, end):
+            if start > end:
                 return None
+            
+            val = preorder[start]
+            node = TreeNode(val)
 
-            root = TreeNode(arr[index])
+            ### binary search for first element > val within range
+            ### specified by indices start+1, end+1 (Python slice convention)
+            i = bisect.bisect(preorder, val, start + 1, end + 1)
+            
+            node.left = build(start + 1, i - 1)
+            node.right = build(i, end)
+        
+            return node
+        
+        return build(0, len(preorder)-1)
 
-            index += 1
+"""
+Solution 2b: same, but use linear search instead of binary search.
 
-            root.left = build(arr, root.val) 
-            root.right = build(arr, bound)
+O(n^2) time if use linear search or pass around array indices, or both.
 
-            return root
+O(n) time for first search.
+This is done once for every node that is constructed.
+So total time is at most O(n^2).
 
-        n = len(preorder)
-        index = 0
-        return build(preorder)
+O(h) extra space for recursion
+O(n) extra space for BST that is built
+
+Note: can pass around modified "end" index that is 1 more than regular one.
+"""
+class Solution2b:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        def build(start, end):
+            if start > end:
+                return None
+            
+            val = preorder[start]
+            node = TreeNode(val)
+            
+            ### linear search for index of first number bigger than val
+            i = start + 1
+            while i <= end and preorder[i] < val:
+                i += 1
+
+            node.left = build(start + 1, i - 1)
+            node.right = build(i, end)
+        
+            return node
+        
+        return build(0, len(preorder)-1)
 
 ###############################################################################
 
 if __name__ == "__main__":
-    def test(preorder, comment=None):
-        root = array_to_bt_lc(arr)
-        
-        solutions = [Solution(), Solution2()]
-
-        res = [s.bstFromPreorder(preorder) for s in solutions]
-
+    def test(arr, comment=None):
         print("="*80)
         if comment:
-            print(comment, "\n")
-        print(f"preorder = {preorder}\n")
-        
-        print(f"\nSolutions:")
-        for root in res:
-            print_tree(root)
-            print()
+            print(comment)
 
+        print(f"\npreorder = {arr}\n")
+
+        res = sol.bstFromPreorder(arr)
+
+        print_tree(res)
+        print()
+
+
+    sol = Solution() # preorder recursion on upper bound for subtree.
+    #sol = Solution1b() # same, but pass index as parameter as well
+
+    #sol = Solution2() # preorder recursion on start and end indices; bsearch
+    #sol = Solution2b() # same, but use linear search
         
     comment = "Single node tree"
     arr = [1]
